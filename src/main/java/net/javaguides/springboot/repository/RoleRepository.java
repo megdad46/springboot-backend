955ab559c741ecd2a890f505edc1609bab0432db
Will Cooper Jr <wcooperjr46@gmail.com>
2023-01-23 15:50:54 -0500

    Initial commit

---
+++ src/main/java/net/javaguides/springboot/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package net.javaguides.springboot.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.javaguides.springboot.model.ERole;
+import net.javaguides.springboot.model.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(ERole name);
+}
